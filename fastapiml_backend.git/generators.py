004ded17e8942f0294098fe0551086aa3cdbf8a5
JJ Perez <gurufractal@hotmail.com>
2025-03-28 19:03:09 -0400

    Se actualizó el cuaderno Jupyter y la app FastAPI

--- fastapiml_backend.git/generators.py
+++ fastapiml_backend.git/generators.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 import pandas as pd
+import numpy as np
 
 def preprocesamiento(df):
     """Realiza limpieza y procesamiento de datos de ventas."""
@@ -39,18 +40,44 @@ def generar_metricas_mensuales(df):
         IngresoPromedioItem=('VentaTotal', 'mean'),
         ClientesActivos=('CustomerID', 'nunique')
     ).reset_index()
-    metricas_mensuales_df.to_csv('metricas_mensuales', index=False)
+
+    #identify which users are active by looking at their revenue per month
+    compras_clientes = df.groupby(['CustomerID','InvoiceYearMonth'])['VentaTotal'].sum().reset_index()
+    #create retention matrix with crosstab
+    matriz_retenciones = pd.crosstab(compras_clientes['CustomerID'], compras_clientes['InvoiceYearMonth']).reset_index()
+ 
+    # Seleccionar los meses a analizar
+    months = matriz_retenciones.columns[1:]
+
+    # Crear DataFrame con métricas de retención
+    retenciones_df = pd.DataFrame({
+        'InvoiceYearMonth': months[1:].astype(int),
+        'TotalClientes': [matriz_retenciones[months[i]].sum() for i in range(len(months) - 1)],
+        'ClientesRetenidos': [
+            matriz_retenciones[(matriz_retenciones[months[i+1]] > 0) & (matriz_retenciones[months[i]] > 0)][months[i+1]].sum()
+            for i in range(len(months) - 1)
+        ]
+    })
+    
+    # Asignar datos al DataFrame de métricas mensuales
+    metricas_mensuales_df[['TotalClientes', 'ClientesRetenidos']] = retenciones_df[['TotalClientes', 'ClientesRetenidos']]
+    #metricas_mensuales_df.to_csv('metricas_mensuales', index=False)
     metricas_mensuales_df['InvoiceYearMonth'] = metricas_mensuales_df['InvoiceYearMonth'].astype(str)
+    metricas_mensuales_df = metricas_mensuales_df.replace([np.inf, -np.inf], np.nan).fillna(0)
     return metricas_mensuales_df
 
 def generar_metricas_clientes(df):
     """Calcula métricas de clientes y retorna un DataFrame con resultados."""
     metricas_clientes_df = df.groupby("CustomerID").agg(
-        TotalGasto=('VentaTotal', 'sum'),
+        IngresosTotal=('VentaTotal', 'sum'),
         CantidadCompras=('InvoiceNo', 'nunique'),
-        PromedioGastoCompra=('VentaTotal', 'mean')
+        MontoPromedioItem=('VentaTotal', 'mean')
     ).reset_index()
     #metricas_clientes_df.to_csv('metricas_clientes.csv', index=False)
+    meses_por_clientes_df = df.groupby('CustomerID')['InvoiceYearMonth'].nunique().reset_index(name='MesesDeCompras')
+    metricas_clientes_df = metricas_clientes_df.merge(meses_por_clientes_df, on='CustomerID')
+    metricas_clientes_df['ARPUCliente'] = metricas_clientes_df['IngresosTotal'] / metricas_clientes_df['MesesDeCompras']
+
     return metricas_clientes_df
 
 def generar_metricas_cohortes(df):
@@ -60,7 +87,48 @@ def generar_metricas_cohortes(df):
     #df['InvoiceYearMonth'] = df['InvoiceDate'].dt.to_period("M")
     #cohort_counts = df.groupby(['CohortMonth', 'InvoiceYearMonth']).agg(NClientes=('CustomerID', 'nunique')).reset_index()
     metricas_cohortes_df = df.groupby(['Cohorte']).agg(ClientesCohorte=('CustomerID', 'nunique')).reset_index()
-    #metricas_cohortes_df.to_csv('metricas_cohortes.csv', index=False)
-    return metricas_cohortes_df
+    # Crear la matriz de retención con crosstab y añadir el cohorte de compra
+
+    
+    metricas_clientes_df = pd.DataFrame(generar_metricas_clientes(df))
+    #identify which users are active by looking at their revenue per month
+    compras_clientes = df.groupby(['CustomerID','InvoiceYearMonth'])['VentaTotal'].sum().reset_index()
+    compras_cohorte = pd.crosstab(compras_clientes['CustomerID'], compras_clientes['InvoiceYearMonth']).reset_index()
+    
+    compras_cohorte = compras_cohorte.merge(metricas_clientes_df[['CustomerID', 'InvoiceYearMonth']], on='CustomerID')
+    #create retention matrix with crosstab
+    matriz_retenciones = pd.crosstab(compras_clientes['CustomerID'], compras_clientes['InvoiceYearMonth']).reset_index()
+    months = matriz_retenciones.columns[1:]
+    # Renombrar columnas usando list comprehension
+    compras_cohorte.columns = [f"m_{col}" if col != 'InvoiceYearMonth' else col for col in compras_cohorte.columns]
+
+    # Calcular tasas de retención por cohorte
+    retention_array = []
+    for mes in months:
+        cohort_data = {'ClientesCohorte': compras_cohorte[compras_cohorte['InvoiceYearMonth'] == mes].shape[0]}
+
+        # Si no hay clientes en la cohorte, asignar 0 a la retención
+        if cohort_data['ClientesCohorte'] > 0:
+            cohort_data[mes] = 1  # Retención total en el mes inicial
+            cohort_mask = compras_cohorte['InvoiceYearMonth'] == mes
+            for next_mes in months[months > mes]:
+                # Si no hay clientes retenidos, asignar 0
+                cohort_data[next_mes] = np.round(
+                    compras_cohorte.loc[cohort_mask, f"m_{next_mes}"].gt(0).sum() / cohort_data['ClientesCohorte'], 2
+                ) if cohort_data['ClientesCohorte'] > 0 else 0
+        else:
+            # Si no hay clientes para esta cohorte, asignar 0
+            cohort_data[mes] = 0
+
+        retention_array.append(cohort_data)
+
+        # Crear DataFrame de tasas de retención
+        tasas_retencion_cohortes_df = pd.DataFrame(retention_array, index=months)
+
+        # Mostrar tabla de retención
+        tasas_retencion_cohortes_df
+
+
+    return tasas_retencion_cohortes_df
 
 
